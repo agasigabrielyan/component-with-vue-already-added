66dfc20d900cf5a9f3e6192a9c018571bd52f9c0
agasi <agasi79@list.ru>
2020-08-19 15:00:15 +0500

    first commit

---
+++ component.form.with.vue.already.added/.description.php
@@ -0,0 +1,10 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = array(
+    "NAME" => GetMessage('COMPONENT_NAME'),
+    "DESCRIPTION" => GetMessage('COMPONENT_DESCRIPTION'),
+    "PATH" => array(
+        "ID" => "NAMESPACE_ID",
+        "NAME" => GetMessage("NAMESPACE")
+    )
+);
---
+++ component.form.with.vue.already.added/templates/.default/template.php
@@ -0,0 +1,14 @@
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<div id="vue-application-component"></div>
+<script>
+    let app = new Vue({
+        el:"#vue-application-component",
+        data:{
+            
+        },
+        methods:{
+
+        },
+        template:"<div class='component-wrapper'></div>"
+    });
+</script>
